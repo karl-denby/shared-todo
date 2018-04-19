b41e5e2a261494400d243740e3ecfd513a638035
Karl Denby <karl.denby@gmail.com>
2018-04-19 23:07:36 +0100

    API CR.. started, still need ..UD

--- todo-web.py
+++ todo-web.py
@@ -1,12 +1,27 @@
 from pymongo import MongoClient
-from bottle import route, run, template, response, static_file, debug
+from bottle import route, run, template, static_file, debug, run, default_app
+from bottle import request, response
+from bottle import post, get, put, delete
+import gunicorn
+import json
+
 
 @route('/')
 def test():
     return static_file('vue/index.html', root='.')
 
-@route('/data')
-def todo_list():
+
+@post('/rest')
+def data_create():
+       
+    data = {'Note': 'CRUD is fun!!!'}
+
+    response.headers['Content-Type'] = 'application/json'
+    return json.dumps(data)
+
+
+@get('/rest')
+def data_read():
     connection = 'mongodb://localhost:27017'
     client = MongoClient(connection)    
     db = client['shared']
@@ -19,5 +34,18 @@ def todo_list():
     response.content_type='application/json'
     return str(results)
 
-debug(True)
-run(reloader=True)
+
+@put('/rest/<data>')
+def update_handler(name):
+    pass
+
+
+@delete('/rest/<data>')
+def delete_handler(name):
+    pass
+
+
+app = application = default_app()
+
+if __name__ == '__main__':
+    run(server='gunicorn', host = '127.0.0.1', port = 8000, debug=True, reloader=True)
